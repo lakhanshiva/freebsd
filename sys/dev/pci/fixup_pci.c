388bf7290e28de47c8b6a9453584bff7177b744e
Lakhan Shiva Kamireddy <lakhanshiva@gmail.com>
2018-07-09 17:11:18 +0530

    Update FIXUP to use PCI_MATCH
    Reviewed by: imp, chuck
    Submitted by: Lakhan Shiva Kamireddy <lakhanshiva@gmail.com>
    Sponsored by: Google, Inc. (GSoC 2018)
    Pull Request: https://github.com/bsdimp/freebsd/pull/8
    Url: https://reviews.freebsd.org/D16233

--- sys/dev/pci/fixup_pci.c
+++ sys/dev/pci/fixup_pci.c
@@ -55,13 +55,11 @@ static int	fixup_pci_probe(device_t dev);
 static void	fixwsc_natoma(device_t dev);
 static void	fixc1_nforce2(device_t dev);
 
-static struct fixup_pci_dev {
-	uint32_t devid;
-	const char *description;
-} fixup_pci_devs[] = {
-	{0x12378086, "Intel 82440FX (Natoma)"},
-	{0x01e010de, "nVidia nForce2"},
-	{0, 0},
+struct pci_device_table fixup_pci_devs[] = {
+	{PCI_DEV(0x8086, 0x1237),
+	 PCI_DESCR("Intel 82440FX (Natoma)")},
+	{PCI_DEV(0x10de, 0x01e0),
+	 PCI_DESCR("nVidia nForce2")}
 };
 
 static device_method_t fixup_pci_methods[] = {
@@ -80,24 +78,18 @@ static driver_t fixup_pci_driver = {
 static devclass_t fixup_pci_devclass;
 
 DRIVER_MODULE(fixup_pci, pci, fixup_pci_driver, fixup_pci_devclass, 0, 0);
-MODULE_PNP_INFO("W32:vendor/device;D:#", pci, fixup_pci, fixup_pci_devs,
-    sizeof(fixup_pci_devs[0]), nitems(fixup_pci_devs) - 1);
+PCI_PNP_INFO(fixup_pci_devs);
 
 static int
 fixup_pci_probe(device_t dev)
 {
-	const struct fixup_pci_dev *fixd;
-	uint32_t devid;
-	size_t i;
+	const struct pci_device_table *fixd;
 
-	devid = pci_get_devid(dev);
-	for (i = 0; i < nitems(fixup_pci_devs); i++) {
-		fixd = &fixup_pci_devs[i];
-		if(fixd->devid == devid) {
-			fixwsc_natoma(dev);
-		}
-	}
-	return(ENXIO);
+	fixd = PCI_MATCH(dev, fixup_pci_devs);
+	if (fixd == NULL)
+		return (ENXIO);
+	device_set_desc(dev, fixd->descr);
+	return (BUS_PROBE_DEFAULT);
 }
 
 static void
